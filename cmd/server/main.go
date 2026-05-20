32dcb2106ef7f1e98b0aa1f2ac1a36823faf1451
Dave Wyatt <dlwyatt115@gmail.com>
2026-05-20 12:24:28 -0400

    next lesson

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,9 @@ import (
 	"syscall"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
 const connectionString = "amqp://guest:guest@localhost:5672/"
@@ -18,8 +21,20 @@ func main() {
 	}
 
 	defer func() { _ = conn.Close() }()
-
 	fmt.Printf("connection successful\n")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	err = pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+
+	if err != nil {
+		panic(err)
+	}
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
